6b4a235dae37ca01479dcd1531af5be4799326ce
Josh Kuehn <joshmkuehn@gmail.com>
2017-07-10 17:31:11 -0500

    Added secrets file for api keys

---
+++ TranslationAPIOptimization/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  TranslationAPIOptimization
+//
+//  Created by JoshuaKuehn on 7/10/17.
+//  Copyright © 2017 JoshQNBlog. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets {
+  var apiTranslationKey = ""
+}
